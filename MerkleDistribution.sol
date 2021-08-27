f24556c39a4b87a0a2f6421346fd3dd2ec4eb2e3
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-08-27 20:50:10 +0800

    Update MerkleDistribution.sol

--- MerkleDistribution.sol
+++ MerkleDistribution.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.16;
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Interfaces/IHolderToken.sol";
 
