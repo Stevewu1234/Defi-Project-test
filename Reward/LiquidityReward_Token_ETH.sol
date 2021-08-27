43cfade96b08d7a2f08c1da2540a16259ba171d8
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-08-27 20:49:42 +0800

    Update LiquidityReward_Token_ETH.sol

--- Reward/LiquidityReward_Token_ETH.sol
+++ Reward/LiquidityReward_Token_ETH.sol
@@ -1,6 +1,6 @@
 /** This contract is referring to Synthetix but added new functions*/
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.16;
 
 
 // Inheritance
@@ -8,9 +8,9 @@ import "./RewardDistributionRecipient.sol";
 import "./LPTokenWrapper.sol";
 
 // Libraries
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/Math.sol";
 
 // Internal References
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
